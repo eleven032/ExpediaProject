f9cdd7c736191617f99cf480a464e3151ff95211
Arthur Shao <Arthur_Shao@epam.com>
2020-08-05 16:00:39 +0800

    Integration with Contacts api

--- Backend/App.js
+++ Backend/App.js
@@ -2,11 +2,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./route');
+const cors = require('cors');
+
 // Variable definitions
 const app = express();
 const port = 3000;
 
 // app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(route);
 
